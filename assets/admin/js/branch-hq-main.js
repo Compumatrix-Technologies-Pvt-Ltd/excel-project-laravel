f83190c4220a2759efbfc4ac17387723deb35d0b
Compumatrix-Technologies-Pvt-Ltd <compumatrixtechnologies@gmail.com>
2025-09-19 12:14:50 +0530

    change

--- assets/admin/js/branch-hq-main.js
+++ assets/admin/js/branch-hq-main.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+
+    $.ajaxSetup({
+        headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
     let options = [];
     let currentIndex = 0;
 
@@ -13,13 +20,13 @@ $(document).ready(function () {
     }
 
     $(".supplierDetails").change(function () {
+
         const selectedType = $(this).val();
 
         $.ajax({
-            url: '{{ route("admin.branch.main.getValues") }}',
+            url: ADMINURL +'/branch-main/getValues',
             type: "POST",
             data: {
-                _token: "{{ csrf_token() }}",
                 type: selectedType,
             },
             success: function (response) {
